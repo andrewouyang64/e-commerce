56fc40243fcd72282b9f6cb4982ae919d3c9fd5b
andrewo <andrew.ouyang@hotmail.com>
2022-05-27 04:11:00 +1000

    updates

--- models/index.js
+++ models/index.js
@@ -5,13 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+});
 
 // Categories have many Products
-
+ategory.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag });
 module.exports = {
   Product,
   Category,
